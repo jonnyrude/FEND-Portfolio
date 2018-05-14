167d331e6ddde84c1e36223fb052748493a53ea7
JR <jrkrelix_01@hotmail.com>
2018-05-13 20:29:20 -0600

    CSS Selectors edit for style
    Also fixed open nav sticking when responsively widening viewport.

--- js/jrudeptflio.js
+++ js/jrudeptflio.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     $("#menuB").on('click', function(e) {
-        console.log("event triggered\n", this);
         $("#menu-small").toggleClass('open');
         e.stopPropagation();
         });
